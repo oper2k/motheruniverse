5e915ac78cf686a83ffb11c9cf9c8c556f870bac
oper2k <76915134+oper2k@users.noreply.github.com>
2023-10-05 19:09:42 +0300

    1

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -1,6 +1,5 @@
-package pro.appful.motheruniverse
+package pro.appful.fraubalance
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterFragmentActivity() {}
